f3754a58090dbd9f38d12b0960338bb463a65b10
Yannik Pitcan <pitcany@gmail.com>
2023-12-30 14:02:53 -0800

    post install updated

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -1,39 +1,68 @@
 #!/bin/bash
 
+apt-get install apt-transport-https curl gnupg -yqq
+#apt-get install apt-transport-https curl gnupg
+apt-get install --no-install-recommends software-properties-common dirmngr
+#apt-get install software-properties-common dirmngr
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
+
 echo "Adding PPAs"
 
-echo "deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/" > /etc/apt/sources.list.d/cran.list
-echo "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-11 main" > /etc/apt/sources.list.d/llvm.list
-echo "deb https://dl.bintray.com/sbt/debian /" > /etc/apt/sources.list.d/sbt.list
+echo "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/" > /etc/apt/sources.list.d/cran.list
+
+echo "deb http://apt.llvm.org/jammy/ llvm-toolchain-$(lsb_release -cs) main" >> /etc/apt/sources.list.d/llvm.list
+echo "deb http://apt.llvm.org/jammy/ llvm-toolchain-$(lsb_release -cs)-16 main" >> /etc/apt/sources.list.d/llvm.list
+
+#echo "deb https://dl.bintray.com/sbt/debian /" > /etc/apt/sources.list.d/sbt.list
 echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublimetext.list
+
+# Install our public GPG key to trusted store
+curl https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc | gpg --dearmor > conda.gpg
+install -o root -g root -m 644 conda.gpg /usr/share/keyrings/conda-archive-keyring.gpg
+
+# Check whether fingerprint is correct (will output an error message otherwise)
+gpg --keyring /usr/share/keyrings/conda-archive-keyring.gpg --no-default-keyring --fingerprint 34161F5BF5EB1D4BFBBB8F0A8AEB4F8B29D82806
+
+# Add our Debian repo
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/conda-archive-keyring.gpg] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main" > /etc/apt/sources.list.d/conda.list
+
+#**NB:** If you receive a Permission denied error when trying to run the above command (because `/etc/apt/sources.list.d/conda.list` is write protected), try using the following command instead:
+#echo "deb [arch=amd64 signed-by=/usr/share/keyrings/conda-archive-keyring.gpg] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main" | sudo tee -a /etc/apt/sources.list.d/conda.list
+
+echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
+echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo -H gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/scalasbt-release.gpg --import
+sudo chmod 644 /etc/apt/trusted.gpg.d/scalasbt-release.gpg
 
 add-apt-repository -y ppa:c2d4u.team/c2d4u4.0+
 add-apt-repository -y ppa:git-core/ppa
-add-apt-repository -y ppa:kdenlive/kdenlive
+#add-apt-repository -y ppa:kdenlive/kdenlive
 add-apt-repository -y ppa:kelleyk/emacs
 add-apt-repository -y ppa:libreoffice/ppa
-add-apt-repository -y ppa:linrunner/tlp
+#add-apt-repository -y ppa:linrunner/tlp
 add-apt-repository -y ppa:linuxuprising/java
-add-apt-repository -y ppa:linuxuprising/shutter
+add-apt-repository ppa:shutter/ppa
+#add-apt-repository -y ppa:linuxuprising/shutter
+add-apt-repository ppa:linuxuprising/apps
 add-apt-repository -y ppa:lyx-devel/release
-add-apt-repository -y ppa:nilarimogard/webupd8
+#add-apt-repository -y ppa:nilarimogard/webupd8
 apt-apt-repository -y ppa:openjdk-r/ppa
 add-apt-repository -y ppa:otto-kesselgulasch/gimp
 add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable
-add-apt-repository -y ppa:stebbins/handbrake-releases
+#add-apt-repository -y ppa:stebbins/handbrake-releases
 add-apt-repository -y ppa:sunderme/texstudio
-add-apt-repository -y ppa:teejee2008/ppa
+#add-apt-repository -y ppa:teejee2008/ppa
 add-apt-repository -y ppa:ubuntu-mozilla-daily/ppa
-add-apt-repository -y ppa:ubuntuhandbook1/apps
-add-apt-repository -y ppa:uget-team/ppa
-add-apt-repository -y ppa:webupd8team/y-ppa-manager
+#add-apt-repository -y ppa:ubuntuhandbook1/apps
+#add-apt-repository -y ppa:uget-team/ppa
+#add-apt-repository -y ppa:webupd8team/y-ppa-manager
 
 echo "updating repositories"
 apt-get update
 
 echo "Installing packages"
 
-apt-get install y-ppa-manager git texstudio timeshift lyx r-base r-recommended elpa-ess r-doc-html oracle-java11-installer-local terminator
+apt-get install git texstudio timeshift lyx r-base r-recommended elpa-ess r-doc-html oracle-java17-installer terminator
 
 echo "Finished adding PPAs and installing applications"
 exit 0
